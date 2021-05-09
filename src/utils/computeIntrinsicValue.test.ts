c0ec541b31739a44b03292d19a4a8d4fad30634a
jknack0 <jonathonknack@gmail.com>
2021-05-09 09:34:43 -0700

    finished compute intrinsic value

--- src/utils/computeIntrinsicValue.test.ts
+++ src/utils/computeIntrinsicValue.test.ts
@@ -1,6 +1,8 @@
 import computeIntrinsicValue from './computeIntrinsicValue';
 
-test('Testing the test', () => {
-    const result = computeIntrinsicValue();
-    expect(result).toBe(1);
+describe('Computing intrinsic value of:', () => {
+    test('Apple', () => {
+        const result = computeIntrinsicValue(10, 4.45, .18, .15, .5, 24.5);
+        expect(result).toBeGreaterThanOrEqual(68);
+    });
 });
--- src/utils/computeIntrinsicValue.ts
+++ src/utils/computeIntrinsicValue.ts
@@ -1,5 +1,19 @@
-const computeIntrinsicValue = (): number => {
-    return 1;
+const computeIntrinsicValue = (years: number, epsTtm: number, growthRate: number, minimumReturn: number, marginOfSafety: number, peRatio: number): number => {
+    const epsGrowthList: number[] = [];
+    epsGrowthList.push(epsTtm);
+    for(let previousYear = 0; previousYear < years - 1; previousYear++) {
+        const forecastedEpsGrowth = epsGrowthList[previousYear] + (epsGrowthList[previousYear] * growthRate);
+        epsGrowthList.push(forecastedEpsGrowth);
+    }
+
+    const stockPriceGrowthList: number[] = [];
+    stockPriceGrowthList.push(epsGrowthList[years - 1] * peRatio);
+    for(let previousYear = 0; previousYear < years - 1; previousYear++) {
+        const forcastedStockPrice = stockPriceGrowthList[previousYear] / (1 + minimumReturn);
+        stockPriceGrowthList.push(forcastedStockPrice);
+    }
+
+    return stockPriceGrowthList[years - 1] * marginOfSafety;
 };
 
 export default computeIntrinsicValue;
